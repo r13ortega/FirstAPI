45e6aec864cb1b87bd47c9b9b16f710429069ceb
Rey <reynaldoortega13@gmail.com>
2022-11-21 18:52:52 -0600

    had to figure out how to fix import javax.persistence.* issue, had to add dependency in pom.xml and import it and appears to be working now, also modify application properties

--- firstAPI/src/main/java/org/dream/firstAPI/Models/User.java
+++ firstAPI/src/main/java/org/dream/firstAPI/Models/User.java
@@ -1,10 +1,17 @@
 package org.dream.firstAPI.Models;
-
+import javax.persistence.*;
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column
     private String firstName;
+    @Column
     private String lastName;
+    @Column
     private int age;
+    @Column
     private String occupation;
 
     public long getId() {
---
+++ firstAPI/src/main/java/org/dream/firstAPI/Repo/UserRepo.java
@@ -0,0 +1,6 @@
+package org.dream.firstAPI.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository {
+}

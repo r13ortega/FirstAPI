debef5feff533d0958730631cb65d04fc830c89e
Rey <reynaldoortega13@gmail.com>
2022-11-22 13:05:38 -0600

    able to connect via Postman and create get,post,delete, and update fucntions

--- firstAPI/src/main/java/org/dream/firstAPI/Controllers/APIController.java
+++ firstAPI/src/main/java/org/dream/firstAPI/Controllers/APIController.java
@@ -28,6 +28,22 @@ public class APIController {
         userRepo.save(user);
         return "Saved...";
     }
+    @PutMapping(value = "update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user){
+        User updateUser = userRepo.findById(id).get();
+        updateUser.setFirstName(user.getFirstName());
+        updateUser.setLastName(user.getLastName());
+        updateUser.setOccupation(user.getOccupation());
+        updateUser.setAge(user.getAge());
+        userRepo.save(updateUser);
+        return "Updated.....";
+    }
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id){
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Delete User with the ID: " +id;
+    }
 
     @GetMapping( "/hello/")
     public static String getPage(){

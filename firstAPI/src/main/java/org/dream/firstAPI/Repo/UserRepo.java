9a696ecb36746da580ca21d0a78a0450b623f267
Rey <reynaldoortega13@gmail.com>
2022-11-21 20:21:31 -0600

    added <User, Long>to UserRepo Class

--- firstAPI/src/main/java/org/dream/firstAPI/Repo/UserRepo.java
+++ firstAPI/src/main/java/org/dream/firstAPI/Repo/UserRepo.java
@@ -1,6 +1,8 @@
 package org.dream.firstAPI.Repo;
 
+import org.dream.firstAPI.Models.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository {
+public interface UserRepo extends JpaRepository <User, Long>{
 }
